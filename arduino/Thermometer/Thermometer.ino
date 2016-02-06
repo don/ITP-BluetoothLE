c14764bfb73591e0245f8591fbaebbd7a8256ec6
Don Coleman <dcoleman@chariotsolutions.com>
2016-02-06 14:13:42 -0500

    Blend

--- arduino/Thermometer/Thermometer.ino
+++ arduino/Thermometer/Thermometer.ino
@@ -13,9 +13,9 @@ BLEService thermometerService = BLEService("BBB0");
 BLEFloatCharacteristic temperatureCharacteristic = BLEFloatCharacteristic("BBB1", BLERead | BLENotify);
 BLEDescriptor temperatureDescriptor = BLEDescriptor("2901", "degrees C");
 
-//#define TEMPERATURE_PIN A0 // RedBear Blend
+#define TEMPERATURE_PIN A0 // RedBear Blend
 // #define TEMPERATURE_PIN A4 // RedBear Nano
-#define TEMPERATURE_PIN 2  // RFduino
+// #define TEMPERATURE_PIN 2  // RFduino
 
 long previousMillis = 0;  // will store last time temperature was updated
 long interval = 2000;     // interval at which to read temperature (milliseconds)
@@ -23,7 +23,7 @@ void setup()
 {
   Serial.begin(9600);
   Serial.println(F("Bluetooth Low Energy Thermometer"));
-  
+
   // set advertised name and service
   blePeripheral.setLocalName("Thermometer");
   blePeripheral.setDeviceName("Thermometer");
@@ -33,7 +33,7 @@ void setup()
   blePeripheral.addAttribute(thermometerService);
   blePeripheral.addAttribute(temperatureCharacteristic);
   blePeripheral.addAttribute(temperatureDescriptor);
-  
+
   blePeripheral.begin();
 }
 
@@ -41,7 +41,7 @@ void loop()
 {
   // Tell the bluetooth radio to do whatever it should be working on
   blePeripheral.poll();
-  
+
   // limit how often we read the sensor
   if(millis() - previousMillis > interval) {
     pollTemperatureSensor();
@@ -67,8 +67,8 @@ float calculateTemperature()
   // read the sensor value
   int sensorValue = analogRead(TEMPERATURE_PIN);
 
-  //float voltage = sensorValue * 5.0; // RedBear Blend
-  float voltage = sensorValue * 3.3; // RedBear Nano & RFduino
+  float voltage = sensorValue * 5.0; // RedBear Blend
+  //float voltage = sensorValue * 3.3; // RedBear Nano & RFduino
   voltage /= 1024.0;
   float temperature = (voltage - 0.5) * 100; // 100 degrees per volt with 0.5 volt offset
 
