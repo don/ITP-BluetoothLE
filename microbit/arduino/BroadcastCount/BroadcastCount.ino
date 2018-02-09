3d00ec2586977dd6f6f46368167d6df80a173d5b
Don Coleman <dcoleman@chariotsolutions.com>
2018-02-09 14:17:36 -0500

    broadcast

---
+++ microbit/arduino/BroadcastCount/BroadcastCount.ino
@@ -0,0 +1,38 @@
+// Broadcast Characteristic Value
+// Simple counter that broadcasts a value
+#include <BLEPeripheral.h>
+
+uint8_t count = 0;
+unsigned long previousMillis = 0;  // will store last time counter was updated
+unsigned short interval = 1000;    // interval at which to update counter (milliseconds)
+
+BLEPeripheral peripheral;
+BLEService service = BLEService("EEE0");
+BLEShortCharacteristic characteristic = BLEShortCharacteristic("EEE1", BLERead | BLENotify | BLEBroadcast);
+
+void setup() {
+  Serial.begin(9600);
+
+  peripheral.setLocalName("BLEBroadcast");
+  peripheral.setAdvertisedServiceUuid(service.uuid());
+  
+  peripheral.addAttribute(service);
+  peripheral.addAttribute(characteristic);
+  
+  characteristic.setValue(count);
+
+  peripheral.begin();
+  characteristic.broadcast();
+
+  Serial.println(F("BLE Broadcast Count"));
+}
+
+void loop() {
+    peripheral.poll();
+    if (millis() - previousMillis > interval) {
+       Serial.println(count);
+      characteristic.setValue(count);    
+      count++;
+      previousMillis = millis();
+    }
+}
